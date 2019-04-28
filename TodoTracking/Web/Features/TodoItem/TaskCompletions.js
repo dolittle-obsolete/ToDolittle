79e54fabea35870fe1243ce8dd3e4a949400fdaf
Einar Ingebrigtsen <einar@dolittle.com>
2019-04-28 08:50:30 +0200

    New artifact that was not generated

---
+++ TodoTracking/Web/Features/TodoItem/TaskCompletions.js
@@ -0,0 +1,18 @@
+﻿/*---------------------------------------------------------------------------------------------
+ *  This file is an automatically generated ReadModel Proxy
+ *  
+ *--------------------------------------------------------------------------------------------*/
+import { ReadModel } from  '@dolittle/readmodels';
+
+export class TaskCompletions extends ReadModel
+{
+    constructor() {
+        super();
+        this.artifact = {
+           id: 'dbe349f8-5582-4c15-b394-4fcd1c0cd874',
+           generation: '1'
+        };
+        this.id = '00000000-0000-0000-0000-000000000000';
+        this.taskCompletion = [];
+    }
+}
